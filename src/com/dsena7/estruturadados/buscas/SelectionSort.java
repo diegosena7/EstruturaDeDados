e89210916e50b98d4e3d27f75f4ece456de02ea5
diegosena7 <diego.tjdosantos@gmail.com>
2022-12-27 12:21:25 -0300

    update selection sort

--- src/com/dsena7/estruturadados/buscas/SelectionSort.java
+++ src/com/dsena7/estruturadados/buscas/SelectionSort.java
@@ -9,30 +9,35 @@ package com.dsena7.estruturadados.buscas;
 public class SelectionSort {
 
 	public static void main(String[] args) {
-		
+
 		int[] vetor = new int[10];
-		
-		for (int i = 0; i < vetor.length; i++) {//Preenche vetor com números aleatórios
+
+		for (int i = 0; i < vetor.length; i++) {
 			vetor[i] = (int) (Math.random() * vetor.length);
 		}
-		
 
 		System.out.println("Desordenado");
 		for (int i = 0; i < vetor.length; i++) {
-			 System.out.println(vetor[i]);
+			System.out.println(vetor[i]);
 		}
-		
-		int posicao_menor = 0, aux;
-		
-		for (int i = 0; i < vetor.length; i++) {
-			for (int j = i + 1; j < vetor.length; i++) {
-				if(vetor[j] < vetor[posicao_menor]) {
+
+		// Selection sort O(N^2)
+		int posicao_menor, aux;
+		for (int i = 0; i < vetor.length; i++) { // O(N)
+			posicao_menor = i;
+			for (int j = i + 1; j < vetor.length; j++) { // O(N)
+				if (vetor[j] < vetor[posicao_menor]) {
 					posicao_menor = j;
 				}
 			}
 			aux = vetor[posicao_menor];
 			vetor[posicao_menor] = vetor[i];
 			vetor[i] = aux;
+		}
+
+		System.out.println("\nOrdenado");
+		for (int i = 0; i < vetor.length; i++) {
+			System.out.println(vetor[i]);
 		}
 	}
 }
